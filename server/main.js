d1986cacc5baee3a37fb51a496488f2ba3e2fba3
Abhishek Patil <abhishek@thezeroth.net>
2013-04-04 23:50:34 +0200

    Basic directory structure and http test server
    Signed-off-by: Abhishek Patil <abhishek@thezeroth.net>

---
+++ server/main.js
@@ -0,0 +1,33 @@
+/* ============================================================
+ *
+ * This file is a part of CodeNameX project
+ * http://thezeroth.net
+ *
+ * Date        : 04-04-2013
+ * Description : Server Application Launch.
+ *
+ * Copyright (C) 2013      by Abhishek patil <abhishek@thezeroth.net>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+// It's a http server
+var http = require("http");
+
+var server = http.createServer( function( req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("It works!");
+});
+
+server.listen(8080);
+console.log("Server is listing on port 8080");
